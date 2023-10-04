60cee9c3b964719bf0c041e50aa5d3053b849df9
gabrielguidini <gabrielsguidini@gmail.com>
2023-10-04 13:41:42 -0300

    removing some code

--- src/main/java/br/com/dasa/repository/ExamRepository.java
+++ src/main/java/br/com/dasa/repository/ExamRepository.java
@@ -4,8 +4,6 @@ import br.com.dasa.model.Exam;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//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
-
 @Repository
 public interface ExamRepository extends JpaRepository<Exam,Long> {
 }
